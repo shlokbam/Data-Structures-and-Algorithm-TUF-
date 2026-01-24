0383a625704e51dd45fbda50f9a5c96aa5177576
Shlok Bam <shlok.bam23@vit.edu>
2026-01-24 09:38:19 +0530

    FIRSTLASTOCCURRENCE

--- Advanced/BinarySearch/LogicBuilding/FirstLastOccurrence.java
+++ Advanced/BinarySearch/LogicBuilding/FirstLastOccurrence.java
@@ -1,24 +1,24 @@
-// class Solution {
-//     public int[] searchRange(int[] nums, int target) {
-//         int n = nums.length;
-//         int first = -1;
-//         int last = -1;
-//         int[] ans = new int[2];
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+        int first = -1;
+        int last = -1;
+        int[] ans = new int[2];
 
-//         for(int i = 0; i< n; i++){
-//             if(nums[i]==target){
-//                 if(first==-1){
-//                     first = i;
-//                 }
-//                     last = i;
-//             }
-//         }
-//         ans[0] = first;
-//         ans[1] = last;
-//         return ans;
+        for(int i = 0; i< n; i++){
+            if(nums[i]==target){
+                if(first==-1){
+                    first = i;
+                }
+                    last = i;
+            }
+        }
+        ans[0] = first;
+        ans[1] = last;
+        return ans;
 
-//     }
-// }
+    }
+}
 
 
 class Solution{
